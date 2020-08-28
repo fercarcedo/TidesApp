8c3398cc17383a8610196e020374893681a910bc
Fernando García Álvarez <fercarcedo@gmail.com>
2020-08-28 23:40:41 +0200

    Added Navigation component and MainActivity with bottom navigation

--- app/src/main/java/fergaral/tidesapp/MainActivity.kt
+++ app/src/main/java/fergaral/tidesapp/MainActivity.kt
@@ -2,17 +2,11 @@ package fergaral.tidesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import fergaral.tidesapp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
     }
 }
