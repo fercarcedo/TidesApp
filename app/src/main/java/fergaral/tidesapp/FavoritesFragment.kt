7ec6cc215a13ee6f67747f7df89c021d2d21053d
Fernando García Álvarez <fercarcedo@gmail.com>
2020-08-29 18:19:14 +0200

    Added initial FavoritesFragment

---
+++ app/src/main/java/fergaral/tidesapp/FavoritesFragment.kt
@@ -0,0 +1,19 @@
+package fergaral.tidesapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import fergaral.tidesapp.databinding.FragmentFavoritesBinding
+
+class FavoritesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
--- app/src/main/java/fergaral/tidesapp/MainActivity.kt
+++ app/src/main/java/fergaral/tidesapp/MainActivity.kt
@@ -3,6 +3,7 @@ package fergaral.tidesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import fergaral.tidesapp.databinding.MainActivityBinding
 
@@ -10,6 +11,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        val binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
