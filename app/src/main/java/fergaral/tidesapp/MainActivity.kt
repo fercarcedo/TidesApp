cfef9d372a3782035c4c5ee6c0b1bee98c58d170
Fernando García Álvarez <fercarcedo@gmail.com>
2022-02-08 00:03:27 +0100

    Added initial ports grid with sample data

--- app/src/main/java/fergaral/tidesapp/MainActivity.kt
+++ app/src/main/java/fergaral/tidesapp/MainActivity.kt
@@ -3,16 +3,23 @@ package fergaral.tidesapp
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
@@ -78,11 +85,30 @@ class MainActivity : AppCompatActivity() {
             Surface(color = MaterialTheme.colors.background, modifier = Modifier.padding(innerPadding)) {
                 Column {
                     val currentScreen = screens.find { it.route == navBackStackEntry?.destination?.route }
-                    Text(style = MaterialTheme.typography.h2, text = currentScreen?.let { stringResource(it.resourceId) } ?: "")
+                    Text(style = MaterialTheme.typography.h2, text = currentScreen?.let { stringResource(it.resourceId) } ?: "", modifier = Modifier.padding(16.dp))
                     NavHost(navController = navController, startDestination = Screen.Ports.route) {
-                        composable(Screen.Ports.route) {}
+                        composable(Screen.Ports.route) {
+                            Ports()
+                        }
                         composable(Screen.Favorites.route) {}
                         composable(Screen.Tides.route) {}
+                    }
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalFoundationApi::class)
+    @Composable
+    fun Ports() {
+        LazyVerticalGrid(
+            cells = GridCells.Adaptive(minSize = 180.dp)
+        ) {
+            items(20) {
+                Card(modifier = Modifier.padding(16.dp)) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Image(painterResource(R.drawable.img), null, contentScale = ContentScale.FillWidth, modifier = Modifier.fillMaxWidth())
+                        Text("Navia", modifier = Modifier.padding(8.dp))
                     }
                 }
             }
